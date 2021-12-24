55378dcc2af1ee49f87a9a13ed3a149fb1e032fa
Pratham <prathambmore2002@gmail.com>
2021-12-24 18:40:39 +0530

    Function Separation

---
+++ photos/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.urls import path
+from django.conf.urls import url 
+from django.views.static import serve
+from . import views
+
+urlpatterns = [
+    path('',views.gallery , name='gallery'),
+    path('photo/<int:pk>/',views.photo , name='photo'),
+    path('delete/<int:pk>/',views.delete , name='delete'),
+    url(f'^download/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
+    path('add/',views.add , name='add'),
+]
---
+++ photos/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+
+from photoalbum.settings import MEDIA_ROOT
+from .models import Category,Photos
+
+from django.conf import settings
+import os
+
+
+# Create your views here.
+
+def gallery(req) : 
+    category = req.GET.get('category','None')
+    category = category[:len(category)]
+    # type = req.GET.get('type','-1')
+    categories = Category.objects.all()
+    photos = Photos.objects.all()
+    
+   
+    # data = {'category':category , 'img_type' : type}
+    # print(type)
+    
+    if category != 'None' : 
+        category = Category.objects.get(name=category)
+        photos = Photos.objects.filter(category=category)
+    # if type != '-1' : 
+    #     photos = photos.filter(type=type)
+    categories = []
+    for photo in Photos.objects.all() : 
+        categories.append(photo.category)
+    context = {'categories' : categories , 'photos' : photos }
+    return render(req,'photos/gallery.html',context)
+
+
+def add(req) : 
+    categories = Category.objects.all()
+    
+    if req.method == 'POST' : 
+        data = req.POST
+        image = req.FILES.get('image')
+        name = data['name']
+        convert = data['convert']
+        
+        if data['category'] != 'none' : 
+            category =  Category.objects.get(id=data['category']) 
+        elif data['new_category'] != '' :
+            category,created = Category.objects.get_or_create(name = data['new_category'])
+        else : 
+            category = None
+        
+       
+        photo = Photos.objects.create(
+            name = name ,
+            category = category ,
+            image = image , 
+            type = convert
+        )
+        
+        return redirect('gallery')
+        
+    context = {'categories' : categories}
+    return render(req,'photos/add.html',context)
+
+def photo(req,pk) : 
+    photo = Photos.objects.get(id=pk)
+    context = {'photo' : photo}
+    print(photo.name)
+    return render(req,'photos/photo.html',context)
+
+
+def delete(req,pk) : 
+    photo = Photos.objects.get(id=pk)
+    print(photo.image)
+    
+    os.remove("static/images/"+str(photo.image))
+    photo.delete()
+    
+    return redirect("gallery")
+
+# def download(request,path) :
+   
