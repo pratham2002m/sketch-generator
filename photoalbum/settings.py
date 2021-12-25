3571eca5ea7be4744e9717b07021664c4e3ca18c
Pratham <prathambmore2002@gmail.com>
2021-12-25 10:26:06 +0530

    Change 6

--- photoalbum/settings.py
+++ photoalbum/settings.py
@@ -129,7 +129,7 @@ STATICFILES = [
 ]
 
 MEDIA_ROOT =  os.path.join(BASE_DIR,'static/images')
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
