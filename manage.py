321b1667de076780ebbff09461a99fb0a5777ea6
Pratham <prathambmore2002@gmail.com>
2021-12-25 14:57:26 +0530

    Allowed Hosts

--- manage.py
+++ manage.py
@@ -15,7 +15,11 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    try:
+        execute_from_command_line(sys.argv) # just put this in try block
+    except:
+          pass
+    # execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
