1b32d0214fdeac1ba9499dd2d5230ca52d5040a5
Pratham <prathambmore2002@gmail.com>
2022-10-12 14:50:15 +0530

    First Comment:

--- photos/urls.py
+++ photos/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.urls import path
-from django.conf.urls import url 
+from django.urls import re_path 
 from django.views.static import serve
 from . import views
 
@@ -8,6 +8,6 @@ urlpatterns = [
     path('',views.gallery , name='gallery'),
     path('photo/<int:pk>/',views.photo , name='photo'),
     path('delete/<int:pk>/',views.delete , name='delete'),
-    url(f'^download/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
+    re_path(f'^download/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
     path('add/',views.add , name='add'),
 ]
